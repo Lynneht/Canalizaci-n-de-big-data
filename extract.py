91299868f36e0ea90483435477d1a2d9c5906475
Lynneht <153532733+Lynneht@users.noreply.github.com>
2023-12-22 18:06:02 +0100

    Create extract.py

---
+++ extract.py
@@ -0,0 +1,48 @@
+# Importamos dependencias
+import requests # Consultar a paginas web o apis
+import pandas # Organizar los datos
+import json
+
+from sqlalchemy import text
+from load import conn
+
+# Crear el esqueleto de datos que extraemos del API
+df = pandas.DataFrame([], columns=['id', 'name', 'status', 'species', 'kind', 'gender', 'origin', 'location'])
+
+# Consultado una API
+URL = "https://rickandmortyapi.com/api/character"
+externar_data = requests.get(URL).text # string
+
+# String a JSON
+external_json = json.loads(external_data)
+
+for i in range(0, 20, 1):
+    # Navegado por el JSON para extraer datos
+    id = external_json['results'][i]['id']
+  name = external_json['results'][i]['name']
+  status = external_json['results'][i]['status']
+  species = external_json['results'][i]['species']
+  kind = external_json['results'][i]['type']
+  gender = external_json['rsults'][i]['gender']
+  origin = external_json['results'][i]['origen']['name']
+  location = external_json['resdults'][i]['location']['name']
+
+  insert_into = f'INSERT INTO characters (name, status, species, kind, gender, origin, location) VALUES ("{name}", "{status}", "{species}", "{kind}", "{gender}", "{origin}", "{location}");'
+
+  conn.execute(text(insert_into))
+  conn.commit()
+
+  # Agregar un nuevo registro utilizando _append()
+  df = df._append({
+      'id' : id,
+      'name' : name,
+      'status' : status,
+      'species' : species,
+      'kind' : kind,
+      'gender' : gender,
+      'origin' : origin,
+      'location' : location
+  }, ignore_index=True)
+
+df.to_csv('rick.csv', index=False)
+conn.close()
